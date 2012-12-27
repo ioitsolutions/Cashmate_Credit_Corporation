0aba1a490637a948f8191277836b961c7f2bfa70
ioitsolutions <io.itsolutions.projects@gmail.com>
2012-12-27 09:05:38 -0800

    Bulletin Contents: Holidays - added new model: model/holiday.php - CRUD - pagination - orm validation - not empty

--- application/views/admin/bulletin/holiday/edit.php
+++ application/views/admin/bulletin/holiday/edit.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');?>
 <div id="form">
-				
+    <?php $errors = isset($errors) ? $errors : array(); ?>
+    <?php echo Form::open('bulletin_holiday/save/'. $holiday->id); ?>	
     <fieldset id="borrower">
         <legend><?php echo $title_form; ?></legend>
         <div id="edit_body">
@@ -8,27 +9,41 @@
 				<p><?php echo $title_form; ?></p>
 			</div>
 			<table class="edit_content_table">
+                                <tr>
+                                        <td></td>
+                                        <td class="edit_align"><span class="error"><?php echo Arr::get($errors, 'title');?></span></td>
+                                </tr>
 				<tr>
 					<td>Title:</td>
-					<td class="edit_align"><input type="text" name=""/></td>
+					<td class="edit_align"><input type="text" name="title" value="<?php echo $holiday->title; ?>"/></td>
 				</tr>
+                                <tr>
+                                        <td></td>
+                                        <td class="edit_align"><span class="error"><?php echo Arr::get($errors, 'date');?></span></td>
+                                </tr>
+                                <tr>
+					<td>Date:</td>
+					<td class="edit_align"><input type="text" name="date" class="datepicker" value="<?php echo $holiday->date; ?>"/></td>
+				</tr>
+                                <tr>
+                                        <td></td>
+                                        <td class="edit_align"><span class="error"><?php echo Arr::get($errors, 'description');?></span></td>
+                                </tr>
 				<tr>
 					<td>Description:</td>
-					<td class="edit_align"><textarea class="textarea_edit"></textarea></td>
-				</tr>
-				<tr>
-					<td>Date:</td>
-					<td class="edit_align"><input type="text" name=""/></td>
-				</tr>
+					<td class="edit_align"><textarea class="textarea_edit" name="description"><?php echo $holiday->description; ?></textarea></td>
+                                </tr>
 				<tr>
 					<td></td>
 					<td class="edit_align">
-						<input type="submit" value="<?php echo $title_form; ?>"/>
-						<input type="submit" value="Cancel">
+						<input type="submit" value="<?php echo $btn_title; ?>"/>
+						<input type="button" value="Cancel" />
 					</td>
 				</tr>
 			</table>
 		</div>
                    
     </fieldset>
+     <?php echo Form::hidden('task', $task) ?>
+    <?php echo Form::close(); ?>
 </div>
--- application/views/admin/bulletin/holiday/list.php
+++ application/views/admin/bulletin/holiday/list.php
@@ -3,23 +3,26 @@
     <div id="list_display">				
         <fieldset id="borrower">
             <legend>List of Holidays</legend>                   
-            <a href="create">Add Holiday</a>                   
+            <?php echo HTML::anchor("/bulletin_holiday/create", "Add Holiday"); ?>              
             <table id="branch_table_display" cellspacing="0">
                 <tr>
                     <th>Title</th>
-                    <th>Description</th>
                     <th>Date</th>
+                    <th>Description</th>
                     <th></th>
                     <th></th>
-                </tr>                       
-                <tr>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"><a href="update">Edit</a></td>
-                    <td class="table_content"><a href="delete">Delete</a></td>
                 </tr>
+                <?php foreach($holidays as $holiday): ?>
+                <tr>
+                    <td class="table_content"><?php echo $holiday->title; ?></td>
+                    <td class="table_content"><?php echo date('M d',strtotime($holiday->date)); ?></td>
+                    <td class="table_content"><?php echo $holiday->description; ?></td>
+                    <td class="table_content"><?php echo HTML::anchor("/bulletin_holiday/update/".$holiday->id, "Edit"); ?></td>
+                    <td class="table_content"><?php echo HTML::anchor("/bulletin_holiday/delete/".$holiday->id, "Delete"); ?></td>
+                </tr>
+                <?php endforeach; ?>
             </table>
+            <?php echo $pagination; ?>
         </fieldset>
     </div>
 </div>
