48ac5e30164b9e52a541dadf31aba23cbf9ccd39
ioitsolutions <io.itsolutions.projects@gmail.com>
2012-12-27 08:56:12 -0800

    Bulletin Contents: Branch Memo - CRUD - pagination - orm validation - not empty

--- application/views/admin/bulletin/memo/branch/edit.php
+++ application/views/admin/bulletin/memo/branch/edit.php
@@ -1,11 +1,12 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');?>
 <div id="form">
-
-    <div>
-        <ul id="nav">
-            <li><?php echo html::anchor('bulletin_memo_employee', 'Employee Memos') ?></li>
-            <li><?php echo html::anchor('bulletin_memo_branch', 'Branch Memos', array('style' => 'background-color: #ffffff;padding: 10px 20px;text-decoration: none;line-height: 2.8em;color:#339900')) ?></li>
-        </ul>
+    <?php $errors = isset($errors) ? $errors : array(); ?>
+    <?php echo Form::open('bulletin_memo_branch/save/'. $memo->id); ?>	
+	<div>
+            <ul id="nav">
+                <li><?php echo html::anchor('bulletin_memo_employee', 'Employee Memos') ?></li>
+                <li><?php echo html::anchor('bulletin_memo_branch', 'Branch Memos', array('style' => 'background-color: #ffffff;padding: 10px 20px;text-decoration: none;line-height: 2.8em;color:#339900')) ?></li>
+            </ul>
 	</div>
     <fieldset id="borrower">
         <legend><?php echo $title_form; ?></legend>
@@ -17,43 +18,44 @@
 				<tr>
 					<td>Type:</td>
 					<td class="edit_align">
-						<select class="selectbox_edit">
-							<option value="">Specific</option>
-							<option value="">Global</option>
-						</select>
+						<?php echo Form::select('type', array('1' => 'Specific','2' => 'Global'), $memo->type,array('class' => 'selectbox_edit')); ?>
 					</td>
 				</tr>
 				<tr>
 					<td>Branch: </td>
 					<td class="edit_align">
-						<select class="selectbox_edit">
-							<option value="">Alaminos</option>
-							<option value="">Angeles</option>
-							<option value="">Dagupan</option>
-						</select>
+						<?php echo Form::select('branch_id', $branches, $memo->branch_id,array('class' => 'selectbox_edit')); ?>
 					</td>
 				</tr>
 				<tr>
 					<td>Title:</td>
-					<td class="edit_align"><input type="text" name=""></td>
+					<td class="edit_align">
+                                            <input type="text" name="title" value="<?php echo $memo->title; ?>"/>
+                                        </td>
 				</tr>
 				<tr>
 					<td>Content: </td>
-					<td class="edit_align"><textarea class="textarea_edit"></textarea></td>
+					<td class="edit_align">
+                                            <textarea name="description" class="textarea_edit"><?php echo $memo->description; ?></textarea>
+                                        </td>
 				</tr>
 				<tr>
 					<td>Expiration Date:</td>
-					<td class="edit_align"><input type="text" name=""></td>
+					<td class="edit_align">
+                                            <input type="text" class="datepicker" name="expiration_date" value="<?php echo $memo->expiration_date; ?>"/>
+                                        </td>
 				</tr>
 				<tr>
 					<td></td>
 					<td class="edit_align">
-						<input type="submit" value="<?php echo $title_form; ?>"/>
-						<input type="submit" value="Cancel">
+						<input type="submit" value="<?php echo $btn_title; ?>"/>
+						<input type="button" value="Cancel" onclick="history.back();" />
 					</td>
 				</tr>
 			</table>
 		</div>
-                  
+                   
     </fieldset>
+    <?php echo Form::hidden('task', $task) ?>
+    <?php echo Form::close(); ?>
 </div>
--- application/views/admin/bulletin/memo/branch/list.php
+++ application/views/admin/bulletin/memo/branch/list.php
@@ -11,7 +11,7 @@
         <fieldset id="borrower">
             <legend>List of Branch Memos</legend>
                   
-            <a href="create">Add Branch Memo</a>
+            <?php echo HTML::anchor("/bulletin_memo_branch/create", "Add Branch Memo"); ?> 
                    
             <table id="branch_table_display" cellspacing="0">
 				<tr>
@@ -25,19 +25,21 @@
                     <th></th>
                     <th></th>
                 </tr>
-                      
+                 <?php foreach($memos as $memo) : ?>
                 <tr>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"></td>
-                    <td class="table_content"><a href="update">Edit</a></td>
-                    <td class="table_content"><a href="delete">Delete</a></td>
+                    <td class="table_content"><?php echo $memo->branch->name; ?></td>
+                    <td class="table_content"><?php echo $memo->title ;?></td>
+                    <td class="table_content"><?php echo $memo->description ;?></td>
+                    <td class="table_content"><?php if($memo->type == 1) echo 'Specific'; else echo 'Global';?></td>
+                    <td class="table_content"><?php echo date('M d, Y',strtotime($memo->expiration_date)) ;?></td>
+                    <td class="table_content"><?php echo date('M d, Y',strtotime($memo->date_created)) ;?></td>
+                    <td class="table_content"><?php echo $memo->user_created ;?></td>
+                    <td class="table_content"><?php echo HTML::anchor("/bulletin_memo_branch/update/".$memo->id, "Edit"); ?> </td>
+                    <td class="table_content"><?php echo HTML::anchor("/bulletin_memo_branch/delete/".$memo->id, "Delete"); ?> </td>
                 </tr>
+                <?php endforeach; ?>
             </table>
+            <?php echo $pagination; ?>
         </fieldset>
     </div>
 </div>
