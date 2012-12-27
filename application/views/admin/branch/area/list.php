6158548d9ed1aa677c5abaa2c5cdd87997a6935b
ioitsolutions <io.itsolutions.projects@gmail.com>
2012-12-27 09:10:22 -0800

    Branch: Areas - added new model: model/area.php - CRUD - pagination - orm validation - not empty

--- application/views/admin/branch/area/list.php
+++ application/views/admin/branch/area/list.php
@@ -3,6 +3,7 @@
     <div id="list_display">
             <fieldset>
                     <legend>List of Areas</legend>
+                    <?php echo HTML::anchor("branch_area/create", "Add Area"); ?>
                     <table id = "branch_table_display" cellspacing="0">
                             <tr>
                                     <th>Branch Name</th>
@@ -12,15 +13,18 @@
                                     <th></th>
                                     <th></th>
                             </tr>
+                            <?php foreach($areas as $area) : ?>
                             <tr>
-                                    <td class="table_content">Urdaneta</td>
-                                    <td class="table_content">000000</td>
-                                    <td class="table_content">Urdaneta</td>
-                                    <td class="table_content">11/11/2012</td>
-                                    <td class="table_content"><a href="update" id="table_content">Edit</a></td>
-                                    <td class="table_content"><a href="delete" id="table_content">Delete</a></td>
+                                    <td class="table_content"><?php echo $area->branch->name; ?></td>
+                                    <td class="table_content"><?php echo $area->code; ?></td>
+                                    <td class="table_content"><?php echo $area->name; ?></td>
+                                    <td class="table_content"><?php echo date('M d, Y',  strtotime($area->effectivity_date)); ?></td>
+                                    <td class="table_content"><?php echo HTML::anchor("branch_area/update/".$area->id, "Edit"); ?></a></td>
+                                    <td class="table_content"><?php echo HTML::anchor("branch_area/delete/".$area->id, "Delete"); ?></td>
                             </tr>
+                            <?php endforeach; ?>
                     </table>
+                    <?php echo $pagination; ?>
             </fieldset>
     </div>
 </div>	
