f7d3702eef3cc67f16b984209d6ccfd7d9f632ec
ioitsolutions <io.itsolutions.projects@gmail.com>
2012-12-27 09:15:13 -0800

    Branch: Bank Accounts - added new model: model/bank.php - CRUD - pagination - orm validation - not empty

--- application/classes/controller/branch/account.php
+++ application/classes/controller/branch/account.php
@@ -6,6 +6,7 @@ class Controller_Branch_Account extends Controller_Template_Admin{
         parent::before();
         $this->template->title = "Cashmate Credit Corporation: Branch Administrator";
         $this->template->styles .= HTML::style('media/styles/admin/branch.css');
+        $this->template->scripts .= HTML::script('media/scripts/admin/bulletin.js');
         $this->template->sublinks = $this->get_branch_links();
     }
    
@@ -14,37 +15,100 @@ class Controller_Branch_Account extends Controller_Template_Admin{
     }
     
     public function action_list(){
-       
+        $banks = ORM::factory('bank');
+        
+        $view = View::factory('admin/branch/account/list');
+        
+        $paginator = new Pagination(array('total_items'=>$banks->count_all()));
+        $paginator->route_params(array('controller' => $this->request->controller(), 'action' => $this->request->action())); 
+        $view->banks = $banks->offset($paginator->offset)->limit($paginator->items_per_page)->find_all()->as_array();
+        $view->pagination = $paginator->render();
+        
         $this->template->title_content = "Branch Administrator";
-        $this->template->content = View::factory('admin/branch/account/list');
+        $this->template->content = $view;
     }
     
     public function action_create(){
-        $view = View::factory('admin/branch/area/edit');
-       
+        $bank = ORM::factory('bank');
+        $branches = ORM::factory('branch')->order_by('name')->find_all()->as_array('id', 'name');
+        
+        $view = View::factory('admin/branch/account/edit');
+        $view->set('bank',$bank);
+        $view->bind('branches', $branches);
+        
         $this->template->title_content = "Branch Administrator: Add Bank Account";
         $view->set('title_form','Add Bank Account');
+        $view->set('btn_title','Add');
+        $view->set('task','Add');
         
         $this->template->content = $view;
     }
     
     public function action_update(){
-        $view = View::factory('admin/branch/area/edit');
-    
+        $bank_id = $this->request->param('id');
+        $bank = ORM::factory('bank',$bank_id);
+        $branches = ORM::factory('branch')->order_by('name')->find_all()->as_array('id', 'name');
+        
+        $view = View::factory('admin/branch/account/edit');
+        $view->set('bank',$bank);
+        $view->bind('branches', $branches);
+        
         $this->template->title_content = "Branch Administrator: Edit Bank Account";
         $view->set('title_form','Edit Bank Account');
-     
+        $view->set('btn_title','Save');
+        $view->set('task','Save');
+        
         $this->template->content = $view;
     }
     
     
     public function action_delete(){
+        $bank_id = $this->request->param('id');
+        $bank = ORM::factory('bank',$bank_id);
         
+        if($bank->loaded()) {
+            //$bank->delete();
+            $this->redirect('branch_account/list/?page=1');
+        }
     }
     
     public function action_save(){
+        $bank_id = $this->request->param('id');
+        $bank = ORM::factory('bank', $bank_id);
+        $bank->values($_POST);
         
+        $errors = array();
+        
+        try{
+            $bank->save();
+            $this->redirect('branch_account/list/?page=1');
+        }catch(ORM_Validation_Exception $ex){
+            $errors = $ex->errors('validation');
+        }
+        
+        $branches = ORM::factory('branch')->order_by('name')->find_all()->as_array('id', 'name');
+        
+        $view = View::factory('admin/branch/account/edit');
+        $view->set("bank", $bank);
+        $view->bind('branches', $branches);
+        $view->set('errors', $errors);
+        
+        if($_POST['task'] == 'Add')
+        {
+            $this->template->title_content = "Branch Administrator: Add Bank Account";
+            $view->set('title_form','Add Bank Account');
+            $view->set('btn_title','Add');
+            $view->set('task','Add');
+        }else{
+            $this->template->title_content = "Branch Administrator: Edit Bank Account";
+            $view->set('title_form','Edit Bank Account');
+            $view->set('btn_title','Save');
+            $view->set('task','Save');
+        }
+        
+        $this->template->content = $view;
     }
     
 }
 ?>
+
--- application/views/admin/branch/account/list.php
+++ application/views/admin/branch/account/list.php
@@ -1,9 +1,10 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');?>
-<div id="form">		
+<div id="form">
 	<div>
 		<div id="list_display">
 			<fieldset>
 				<legend>List of Bank Accounts</legend>
+                                <?php echo HTML::anchor("/branch_account/create", "Add Bank Account"); ?>
 				<table id="branch_table_display" cellspacing="0">
 					<tr>
 						<th>Branch Name</th>
@@ -13,15 +14,18 @@
 						<th>Edit</th>
 						<th>Delete</th>
 					</tr>
+                                        <?php foreach($banks as $bank): ?>
 					<tr>
-						<td class="table_content">Urdaneta</td>
-						<td class="table_content">0000000000000</td>
-						<td class="table_content">Metro Bank</td>
-						<td class="table_content">11/11/2012</td>
-						<td class="table_content"><a href="update" id="table_content">Edit</a></td>
-						<td class="table_content"><a href="delete" id="table_content">Delete</a></td>
+						<td class="table_content"><?php echo $bank->branch->name; ?></td>
+						<td class="table_content"><?php echo $bank->account_no; ?></td>
+						<td class="table_content"><?php echo $bank->name; ?></td>
+                                                <td class="table_content"><?php echo date('M d, Y',  strtotime($bank->effectivity_date)); ?></td>
+						<td class="table_content"><?php echo HTML::anchor("/branch_account/update/".$bank->id, "Edit"); ?></td>
+						<td class="table_content"><?php echo HTML::anchor("/branch_account/delete/".$bank->id, "Delete"); ?></td>
 					</tr>
+                                        <?php endforeach; ?>
 				</table>
+                                <?php echo $pagination; ?>
 			</fieldset>
 		</div>
 	</div>		
