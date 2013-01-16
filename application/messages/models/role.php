1b5569c4dd28fd47d9c81c88ef45368889fafe0d
j-apola <jeff_apola@yahoo.com>
2013-01-15 22:04:49 -0800

    Sql dump for changes made on the database

--- application/messages/models/role.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-return array(
-    'role_password' => array
-        (
-        'required'=>'Employee password requires a value'
-        ),
-);
-?>
